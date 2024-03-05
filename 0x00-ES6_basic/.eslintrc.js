265a8d54cea41a6cec13136db2c361df5f6ff6ca
JoanWaithira <joanwaithira.jw@gmail.com>
2024-03-05 07:23:44 +0000

    Debugging

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,12 +1,36 @@
 module.exports = {
-  extends: 'airbnb-base',
-  plugins: ['import'],
-  rules: {
-    'linebreak-style': 0,
-    'no-console': 'off',
-    'import/extensions': ['error', 'always', {
-      js: 'never',
-    }],
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
   },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
 
